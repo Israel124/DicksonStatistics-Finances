e00cc89916e1ff41669556e25930b8f690a1addc
Luisy1120 <israelquintero113@gmail.com>
2025-11-13 10:59:50 -0600

    4to Cambio

--- DicksonStatistics/urls.py
+++ DicksonStatistics/urls.py
@@ -5,7 +5,7 @@ from django.urls import include
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', views.login_view, name='login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('register/', views.register, name='register'),
